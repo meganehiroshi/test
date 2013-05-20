c977a0fbf0d03ff025be4c455c2e407192d3f2d1
meganehiroshi <kawasakhiroshi@gmail.com>
2013-05-21 01:18:05 +0900

    package.jsonの更新（mysql追加）

--- hello.js
+++ hello.js
@@ -1,11 +1,17 @@
 var text = 'hello world';
 console.log(text);
 
+
+var http = require('http');
+var server = http.createServer();
+
+console.log('URL:' + server.request.headers.host);
+
 var AWS = require('aws-sdk');
- 
+
  AWS.config.loadFromPath('credentials.json');
  AWS.config.update({region: 'ap-northeast-1'});
-  
+
   var s3 = new AWS.S3();
   s3.listBuckets(function(err, data) {
     for (var index in data.Buckets) {
